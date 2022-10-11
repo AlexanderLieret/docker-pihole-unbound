c922cd82f8ac264408ccab89ea4f82d6d46f1bda
Alexander Lieret <alexander.lieret@fau.de>
2022-10-11 15:38:50 +0000

    Import old non-git project

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM "pihole/pihole:2022.05"
+
+RUN apt-get update && apt-get install -y unbound && rm -rf /var/cache/apt/archives /var/lib/apt/lists/*
+RUN ! unbound-anchor -a "/var/lib/unbound/root.key"
+
+COPY root /
+
+ENV PREFER_IPv6=false
+ENV PIHOLE_DNS_=127.0.0.1#5335
---
+++ root/etc/cont-init.d/21-unbound.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/with-contenv bash
+set -e
+
+if [ "${PH_VERBOSE:-0}" -gt 0 ] ; then
+	set -x ;
+fi
+
+if [ "${IPv6,,}" = "true" ] ; then
+	sed -i -e '/do-ip6:/ s/: .*/: yes/' /etc/unbound/unbound.conf.d/pi-hole.conf
+	if [ "${UNBOUND_PREFER_IPv6,,}" = "true" ] ; then
+		sed -i -e '/prefer-ip6:/ s/: .*/: yes/' /etc/unbound/unbound.conf.d/pi-hole.conf
+	else
+		sed -i -e '/prefer-ip6:/ s/: .*/: no/' /etc/unbound/unbound.conf.d/pi-hole.conf
+	fi
+else
+	sed -i -e '/do-ip6:/ s/: .*/: no/' /etc/unbound/unbound.conf.d/pi-hole.conf
+	sed -i -e '/prefer-ip6:/ s/: .*/: no/' /etc/unbound/unbound.conf.d/pi-hole.conf
+fi
